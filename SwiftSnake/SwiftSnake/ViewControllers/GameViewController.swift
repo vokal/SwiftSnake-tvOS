e1f841d25dc878c98f758292fd166f70d07acad9
Ellen Shapiro <designatednerd@gmail.com>
2015-09-24 17:38:44 -0500

    Nuke boilerplate and done TODO

--- SwiftSnake/SwiftSnake/ViewControllers/GameViewController.swift
+++ SwiftSnake/SwiftSnake/ViewControllers/GameViewController.swift
@@ -12,8 +12,6 @@ import SpriteKit
 
 class GameViewController: UIViewController {
 
-    //TODO: Figure out how the hell this actually works and replace this sample code with real code.
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -22,30 +20,8 @@ class GameViewController: UIViewController {
             return;
         }
         
-        print("FRAME: \(skView.frame)")
-        
-        // create a new scene
+        // create the Snake game scene
         let scene = SnakeGameScene.makeSceneOfSize(skView.frame.size)
         skView.presentScene(scene)
-        
-        // add a tap gesture recognizer
-        let tapGesture = UITapGestureRecognizer(target: self, action: "handleTap:")
-        var gestureRecognizers = [UIGestureRecognizer]()
-        gestureRecognizers.append(tapGesture)
-        if let existingGestureRecognizers = skView.gestureRecognizers {
-            gestureRecognizers.appendContentsOf(existingGestureRecognizers)
-        }
-        skView.gestureRecognizers = gestureRecognizers
-    }
-    
-    func handleTap(gestureRecognizer: UIGestureRecognizer) {
-        
-        guard let skView = self.view as? SKView else {
-            return;
-        }
-        
-        // check what nodes are tapped
-        let tapLocation = gestureRecognizer.locationInView(skView)
-        print("Tapped \(tapLocation)")
     }
 }
