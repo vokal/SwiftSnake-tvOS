fd7b318ae6aba45d1e0382312643505c50f582f4
Ellen Shapiro <designatednerd@gmail.com>
2015-09-24 08:28:15 -0500

    Initial project commit.

---
+++ SwiftSnake/SwiftSnakeTests/SwiftSnakeTests.swift
@@ -0,0 +1,18 @@
+//
+//  SwiftSnakeTests.swift
+//  SwiftSnakeTests
+//
+//  Created by Ellen Shapiro (Vokal) on 9/24/15.
+//  Copyright © 2015 Vokal. All rights reserved.
+//
+
+import XCTest
+@testable import SwiftSnake
+
+class SwiftSnakeTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        XCTFail("Y U NO WRITE TESTS?")
+    }
+}
