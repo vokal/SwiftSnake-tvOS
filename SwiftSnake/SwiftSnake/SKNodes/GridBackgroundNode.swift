6e02fafcbc6184e8a6e7867e9d80629cf116a82a
Ellen Shapiro <designatednerd@gmail.com>
2015-09-24 08:53:24 -0500

    Move stuff around and begin creating nodes

---
+++ SwiftSnake/SwiftSnake/SKNodes/GridBackgroundNode.swift
@@ -0,0 +1,14 @@
+//
+//  GridBackgroundNode.swift
+//  SwiftSnake
+//
+//  Created by Ellen Shapiro (Vokal) on 9/24/15.
+//  Copyright © 2015 Vokal. All rights reserved.
+//
+
+import Foundation
+import SpriteKit
+
+class GridBackgroundNode: SKNode {
+
+}
