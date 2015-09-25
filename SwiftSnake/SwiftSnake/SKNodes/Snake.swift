ffa964bfb98f993420486623c9221b0f7ff3f2de
Ellen Shapiro <designatednerd@gmail.com>
2015-09-24 20:07:52 -0500

    More snake logic

--- SwiftSnake/SwiftSnake/SKNodes/Snake.swift
+++ SwiftSnake/SwiftSnake/SKNodes/Snake.swift
@@ -19,23 +19,27 @@ class Snake: SKNode {
     }
 
     let head = SnakeHeadNode()
-    var bodyParts = [SnakeBodyPartNode]()
+    var bodyPartCount = 3 //Start with this many body parts.
     
     //MARK:- User navigation of the snake
     
     func turn(direction: TurnDirection) {
-    
+        switch direction {
+        case .Left:
+            print("TURN LEFT")
+        case .Right:
+            print("TURN RIGHT")
+        case .Up:
+            print("TURN UP")
+        case .Down:
+            print("TURN DOWN FOR WHAT?!")
+        }
     }
     
     //MARK:- Food Consumption
     
     func eatFood() {
         //Add a body part to the end of the snake. 
-        let bodyPart = SnakeBodyPartNode()
-        
-        //Add to end of snake
-        
-        //Add to array
-        bodyParts.append(bodyPart)
+        ++self.bodyPartCount
     }
 }
